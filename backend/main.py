7a994e57b16444fd3f6e2663dfb299c11780bd8c
pinardy <pinardy.yang@gmail.com>
2025-03-23 12:15:06 +0800

    feat: backend fetch data from postgres

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,36 @@
+import os, psycopg2
 from fastapi import FastAPI
+from psycopg2.extras import RealDictCursor
 
 app = FastAPI()
 
-# TODO: create endpoints to expose telematics data from postgres
+def get_connection():
+    # Connect to PostgreSQL database
+    conn = psycopg2.connect(
+        dbname=os.environ["POSTGRES_DB"],
+        user=os.environ["POSTGRES_USER"],
+        password=os.environ["POSTGRES_PASSWORD"],
+        host=os.environ["POSTGRES_HOST"],
+        port=os.environ["POSTGRES_PORT"]
+    )
+    return conn, conn.cursor(cursor_factory=RealDictCursor)
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+
+@app.get("/trip/get_all")
+async def get_all_trips():
+    try:
+        # Connect to the database
+        conn, cursor = get_connection()
+
+        # Query the telematics table
+        cursor.execute("SELECT * FROM telematics;")
+        rows = cursor.fetchall()
+        print(rows)
+
+        # Close the connection
+        cursor.close()
+        conn.close()
+
+        return rows
+    except Exception as e:
+        return {"error": str(e)}
