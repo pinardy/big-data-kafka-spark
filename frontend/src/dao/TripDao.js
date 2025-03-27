0a5609130f0f327da65611a17e941b4787e4ec62
pinardy <pinardy.yang@gmail.com>
2025-03-27 11:01:54 +0800

    refactor: add TODO in frontend main page

--- frontend/src/dao/TripDao.js
+++ frontend/src/dao/TripDao.js
@@ -5,7 +5,6 @@ export const getTripData = async () => {
     const trips = await axios
       .get('http://localhost:8000/trip/get_all')
       .then((response) => response.data)
-    console.log('trips: ', trips)
     return trips ? trips : []
   } catch (error) {
     return []
