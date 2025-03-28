f75498163736fc851f3cef41a0e80f7d3a401bfd
pinardy <pinardy.yang@gmail.com>
2025-03-28 22:10:04 +0800

    feat: add simulate streaming live data

--- streaming-source/main.py
+++ streaming-source/main.py
@@ -1,36 +1,82 @@
-from fastapi import FastAPI, UploadFile, File
-from fastapi.responses import JSONResponse, FileResponse
-from fastapi.staticfiles import StaticFiles
-from typing import List
+import os, json, asyncio
 import pandas as pd
-import io
+from fastapi import FastAPI, HTTPException
+from kafka import KafkaProducer
 
 app = FastAPI()
 
-app.mount("/static", StaticFiles(directory="static"), name="static")
+KAFKA_BROKER = os.environ["KAFKA_BROKER"]
+KAFKA_TOPIC = os.environ["KAFKA_TOPIC"]
 
-@app.get("/")
-async def read_root():
-    return FileResponse("static/upload.html")
+# KAFKA_BROKER = "localhost:9092"
+# KAFKA_TOPIC = "streaming"
 
-@app.post("/upload-multiple/")
-async def upload_multiple(files: List[UploadFile] = File(...)):
-    combined_data = {}
+data_file_path = "./part-00000-e6120af0-10c2-4248-97c4-81baf4304e5c-c000.csv"
 
-    for file in files:
-        contents = await file.read()
-        try:
-            df = pd.read_csv(io.BytesIO(contents))
-        except Exception as e:
-            return JSONResponse(status_code=400, content={"error": f" {file.filename} fail: {str(e)}"})
+# Simulating streaming data from a CSV file (using pandas)
+df = pd.read_csv(data_file_path)
 
-        if "bookingID" not in df.columns:
-            return JSONResponse(status_code=400, content={"error": f" {file.filename} lost 'bookingID' column"})
+# Sort data by "bookingID" and "second" for grouping trips in order of occurrence
+df = df.sort_values(by=["bookingID", "second"])
 
-        for booking_id, group in df.groupby("bookingID"):
-            tid = str(booking_id)
-            if tid not in combined_data:
-                combined_data[tid] = []
-            combined_data[tid].extend(group.to_dict(orient="records"))
+# Group by "bookingID" and collect the records as a list of dictionaries
+grouped_data = (
+    df.groupby("bookingID")
+    .apply(lambda x: x.to_dict(orient="records"))
+    .to_dict()
+)
 
-    return JSONResponse(content=combined_data)
+
+def create_producer(broker):
+    producer = KafkaProducer(
+        bootstrap_servers=[broker],
+        value_serializer=lambda v: json.dumps(v).encode('utf-8')  # Serialize messages to JSON
+    )
+    return producer
+
+
+async def send_message(producer, topic, message):
+    try:
+        # producer.send(topic, message)
+        # Use asyncio.to_thread to run the synchronous producer.send in a separate thread
+        print(f"Sent: bookingID': {message.get('bookingID')}, 'second': {message.get('second')}")
+        await asyncio.to_thread(producer.send, topic, message)
+
+    except Exception as e:
+        print(f"Failed to send message: {e}")
+
+
+async def stream_booking_trips(producer, trips):
+    for trip in trips:
+        await send_message(producer, KAFKA_TOPIC, trip)
+        await asyncio.sleep(2)
+
+
+# Run "curl -X POST http://localhost:8000/stream_trips" to start streaming data to Kafka
+@app.post("/stream_trips")
+async def stream_trips():
+    """
+    Streams trip data to Kafka topic to simulate many drivers sending trip data in real-time
+    """
+    try:
+        producer = create_producer(KAFKA_BROKER)
+
+        tasks = []
+        for i, (booking_id, trips) in enumerate(grouped_data.items()):
+            task = asyncio.create_task(stream_booking_trips(producer, trips))
+            tasks.append(task)
+
+            if i == 2:
+                break
+
+        # Run all tasks concurrently
+        await asyncio.gather(*tasks)
+        return { "message": "Streaming to Kafka completed successfully" }
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+# Shutdown hook to close the Kafka producer
+@app.on_event("shutdown")
+def shutdown_event(producer: KafkaProducer):
+    producer.close()
