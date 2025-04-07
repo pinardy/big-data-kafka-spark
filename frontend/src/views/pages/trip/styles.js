0a9ee8e51831721ed1990ae91bdc490182db5939
Pinardy <pinardy.yang@gmail.com>
2025-04-07 11:17:43 +0800

    feat: add websocket for live streaming to frontend (#16)
    * feat: add websocket  to backend server
    
    * feat: add websocket client to frontend

---
+++ frontend/src/views/pages/trip/styles.js
@@ -0,0 +1,51 @@
+export const styles = {
+  container: {
+    fontFamily: 'Arial, sans-serif',
+    maxWidth: '400px',
+    margin: '0 auto',
+    padding: '20px',
+    backgroundColor: '#f9f9f9',
+    borderRadius: '8px',
+    boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1)',
+  },
+  header: {
+    textAlign: 'center',
+    marginBottom: '20px',
+  },
+  title: {
+    fontSize: '24px',
+    color: '#4CAF50',
+  },
+  main: {
+    display: 'flex',
+    flexDirection: 'column',
+    gap: '20px',
+  },
+  tripDetails: {
+    backgroundColor: '#fff',
+    padding: '15px',
+    borderRadius: '8px',
+    boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)',
+  },
+  driverDetails: {
+    backgroundColor: '#fff',
+    padding: '15px',
+    borderRadius: '8px',
+    boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)',
+  },
+  sectionTitle: {
+    fontSize: '18px',
+    marginBottom: '10px',
+    color: '#333',
+  },
+  button: {
+    padding: '10px 20px',
+    fontSize: '16px',
+    color: '#fff',
+    backgroundColor: '#4CAF50',
+    border: 'none',
+    borderRadius: '8px',
+    cursor: 'pointer',
+    textAlign: 'center',
+  },
+}
