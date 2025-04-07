b6aecba2acfeddac5a9203e20e162a50ad6b64e1
Desmond Tan <desmondtzh@hotmail.com>
2025-04-07 22:27:13 +0800

    Revert "feat: fix the issues in streaming"
    This reverts commit 99775914d1fb01c5112f79a3013b1cdad30684b7.

--- spark-server/spark-tasks.sh
+++ spark-server/spark-tasks.sh
@@ -3,5 +3,3 @@
 spark-submit batch_processing.py > /tmp/batch_processing.log &
 
 spark-submit telematics_consolidate.py > /tmp/telematics_consolidate.log &
-
-spark-submit streaming-data-handling.py > /tmp/streaming-data-handling.log &
