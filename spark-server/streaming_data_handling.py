fd49412e95f7de591a5b4778f5667425974aee47
Desmond Tan <46779933+desmondTzh@users.noreply.github.com>
2025-04-21 22:00:13 +0800

    feat: add kafka and fastapi module to run concurrently and update model on the fly (#26)
    * WIP: refactoring stream to allow 2 thread(FastAPI and Kafka) to run
    
    * add multi thread but yet unable to get model shared between kafka and fastapi
    
    * update streaming data for kafka+fastapi

--- spark-server/streaming_data_handling.py
+++ spark-server/streaming_data_handling.py
@@ -1,17 +1,20 @@
+import asyncio
+import os, json, time, uvicorn
+import threading
+
+from typing import Optional
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import col, stddev, max, sqrt, mean
 from kafka import KafkaConsumer, KafkaProducer
-import os, json, time, uvicorn
 from fastapi import FastAPI
 from pydantic import BaseModel
-
 from collections import defaultdict, deque
+from minio import Minio
 
 from pyspark.ml.classification import RandomForestClassificationModel
 from pyspark.ml.feature import VectorAssembler
-from minio import Minio
 
-FAST_API_PORT = int(os.environ.get("FAST_API_MODEL_PREDICT_PORT", 8004))
+FAST_API_PORT = int(os.environ.get("FAST_API_MODEL_PREDICT_PORT", 8005))
 
 # MinIO Connection
 MINIO_ADDRESS = os.environ["MINIO_ADDRESS"]
@@ -24,22 +27,122 @@ KAFKA_BROKER = os.environ["KAFKA_BROKER"]
 KAFKA_TOPIC_INPUT = os.environ["KAFKA_TOPIC_STREAMING"]
 KAFKA_TOPIC_OUTPUT = os.environ["KAFKA_TOPIC_PREDICTION"]
 
+##### METHOD 1: with shared variable
+# shared_variable = {"model": None,"metadata":None}
+# shared_lock = threading.Lock()
+#
+# def get_model():
+#     global shared_variable
+#     with shared_lock:
+#         return shared_variable["model"], shared_variable["metadata"]
+# def set_model(model, metadata):
+#     global shared_variable
+#     with shared_lock:
+#         shared_variable["model"] = model
+#         shared_variable["metadata"] = metadata
+
+
+##### METHOD 2: with static method
+class SharedModel:
+    _instance = None
+    _lock = threading.Lock()
+
+    def __init__(self):
+        self.model = None
+        self.metadata = None
+
+    @staticmethod
+    def get_instance():
+        if SharedModel._instance is None:
+            with SharedModel._lock:
+                if SharedModel._instance is None:
+                    SharedModel._instance = SharedModel()
+        return SharedModel._instance
+
+    def get_model(self):
+        with SharedModel._lock:
+            return self.model, self.metadata
+
+    def set_model(self, model, metadata):
+        with SharedModel._lock:
+            self.model = model
+            self.metadata = metadata
+
+def get_model():
+    return SharedModel.get_instance().get_model()
+
+def set_model(model, metadata):
+    SharedModel.get_instance().set_model(model, metadata)
+
+shared_model = SharedModel.get_instance()  # Initialize the singleton instance
+
+class SparkSessionSingleton:
+    _instance = None
+    _lock = threading.Lock()  # Lock to ensure thread-safe access
+    _lock2 = threading.Lock()  # Lock to ensure thread-safe access
+
+    @staticmethod
+    def get_instance():
+        with SparkSessionSingleton._lock:  # Ensure only one thread can access this block at a time
+            if SparkSessionSingleton._instance is None:
+                SparkSessionSingleton._instance = SparkSession.builder \
+                .appName("PySpark Streaming Processor") \
+                .config("spark.hadoop.fs.s3a.endpoint", f"http://{MINIO_ADDRESS}:{MINIO_PORT}") \
+                .config("spark.hadoop.fs.s3a.access.key", MINIO_USER) \
+                .config("spark.hadoop.fs.s3a.secret.key", MINIO_PASSWORD) \
+                .config("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem") \
+                .config("spark.hadoop.fs.s3a.path.style.access", "true") \
+                .config("spark.hadoop.fs.s3a.connection.ssl.enabled", "false") \
+                .config("spark.sql.adaptive.enabled", "true") \
+                .config("spark.dynamicAllocation.enabled", "false") \
+                .getOrCreate()
+        return SparkSessionSingleton._instance
+    #
+
+    ##### METHOD 3: trying to add model to spark session singleton
+    # _model = None
+    # _metadata = None
+    #
+    # @staticmethod
+    # def get_model():
+    #     with SparkSessionSingleton._lock2:  # Ensure only one thread can access this block at a time
+    #
+    #         print(f"GETTING MODEL: { SparkSessionSingleton._model}, {SparkSessionSingleton._metadata}")
+    #         return SparkSessionSingleton._model,SparkSessionSingleton._metadata
+    #
+    # @staticmethod
+    # def set_model(model, metadata):
+    #     print(f"SETTING MODEL WAS: { SparkSessionSingleton._model}, {SparkSessionSingleton._metadata}")
+    #     if model is None:
+    #         return
+    #     with SparkSessionSingleton._lock2:  # Ensure only one thread can access this block at a time
+    #
+    #         SparkSessionSingleton._model = model
+    #         SparkSessionSingleton._metadata = metadata
+    #         print(f"SETTING MODEL NOW: { SparkSessionSingleton._model}, {SparkSessionSingleton._metadata}")
+
+def generateSparkSession():
+   return SparkSessionSingleton.get_instance()
+
 # Load Model from MinIO
 def load_model_from_minio():
-    minio_client = Minio(
-        f"{MINIO_ADDRESS}:{MINIO_PORT}",
-        access_key=MINIO_USER,
-        secret_key=MINIO_PASSWORD,
-        secure=False
-    )
-    
-    minio_bucket = "models"
-    minio_url = "s3a://models/sparkml_model_v1"  # Adjust if needed
-    metadata_path = "rf_model/metadata_v1.json"
 
-    local_metadata_path = "/tmp/model_metadata_v1.json"
-    
     try:
+        print(f"load model")
+
+        minio_client = Minio(
+            f"{MINIO_ADDRESS}:{MINIO_PORT}",
+            access_key=MINIO_USER,
+            secret_key=MINIO_PASSWORD,
+            secure=False
+        )
+
+        minio_bucket = "models"
+        minio_url = "s3a://models/sparkml_model_v1"  # Adjust if needed
+        metadata_path = "rf_model/metadata_v1.json"
+
+        local_metadata_path = "/tmp/model_metadata_v1.json"
+
         # Download metadata
         minio_client.fget_object(minio_bucket, metadata_path, local_metadata_path)
         print(f"---- Downloaded metadata from {metadata_path} to {local_metadata_path}")
@@ -47,21 +150,30 @@ def load_model_from_minio():
         with open(local_metadata_path, "r") as f:
             metadata = json.load(f)
         print(f"Loaded metadata from {local_metadata_path} successfully")
+        # Ensure SparkSession is initialized
+        spark = generateSparkSession()
+        print(f"GENERATED SPARK SESSION: {spark}")
 
         model = RandomForestClassificationModel.load(minio_url)
         print(f"Loaded model from {minio_url} successfully")
 
         print(f"Model metadata: {metadata[f'feature_columns']}")
-        
+        print(f"load model {model}, {metadata}")
         return model, metadata
-        
+
     except Exception as e:
         print(f"Error loading model: {str(e)}")
-        raise
-
+        return None, None
 # Predict New Data
-def predict_new_data(model, metadata, new_data_df):
+def predict_new_data(new_data_df):
     # new_data_df.printSchema()
+    model, metadata = get_model()
+    if model is None:
+        model, metadata = load_model_from_minio()
+        set_model(model, metadata)
+
+    if model is None:
+        return None
 
     feature_columns = [col for col in new_data_df.columns if col != "label"]
     assembler = VectorAssembler(inputCols=feature_columns, outputCol="features")
@@ -69,8 +181,6 @@ def predict_new_data(model, metadata, new_data_df):
 
     predict_result = model.transform(assembled_data)
 
-    # print("Predict result schema:")
-    # predict_result.printSchema()
     return predict_result.select("prediction").first()[0]
 
 # In-memory buffer for tracking records per bookingID
@@ -81,68 +191,73 @@ WINDOW_SIZE = 10 # Number of records to trigger prediction
 STEP_SIZE = 5 # Number of records to step forward
 
 # Background thread to process stale records
-def cleanup_stale_records(producer, model, metadata, spark):
+def cleanup_stale_records(producer):
     current_time = time.time()
     # print(f"Checking for stale records at {current_time}...")
     expired_bookings = [bid for bid, t in booking_timestamps.items() if current_time - t > TIMEOUT_SECONDS]
     for bid in expired_bookings:
         if len(data_buffer[bid]) > 0:
-            process_and_predict(bid, list(data_buffer[bid]), producer, model, metadata, spark)
+            process_and_predict(bid, list(data_buffer[bid]), producer)
             del data_buffer[bid]
             del booking_timestamps[bid]
             # print(f"Processed stale bookingID: {bid}")
 
 # Process records and make predictions
-def process_and_predict(bookingID, records, producer, model, metadata, spark):
+def process_and_predict(bookingID, records, producer):
+
+    spark = generateSparkSession()
+
     df = spark.createDataFrame(records)
-    
+
     # Calculate instantaneous magnitude features
-    df = df.withColumn("accel_mag", sqrt(col("acceleration_x")**2 + 
-                                        col("acceleration_y")**2 + 
+    df = df.withColumn("accel_mag", sqrt(col("acceleration_x")**2 +
+                                        col("acceleration_y")**2 +
                                         col("acceleration_z")**2)) \
-           .withColumn("gyro_mag", sqrt(col("gyro_x")**2 + 
-                                      col("gyro_y")**2 + 
+           .withColumn("gyro_mag", sqrt(col("gyro_x")**2 +
+                                      col("gyro_y")**2 +
                                       col("gyro_z")**2))
-    
+
+
     processed_df = df.groupBy("bookingid").agg(
         mean("speed").alias("avg_speed"),
         stddev("speed").alias("std_speed"),
-        
+
         mean("accel_mag").alias("avg_accel_mag"),
         max("accel_mag").alias("max_accel_mag"),
         stddev("accel_mag").alias("std_accel_mag"),
-        
+
         mean("gyro_mag").alias("avg_gyro_mag"),
         stddev("gyro_mag").alias("std_gyro_mag"),
-        
+
         mean("acceleration_x").alias("avg_accel_x"),
         stddev("acceleration_x").alias("std_accel_x"),
         max("acceleration_x").alias("max_accel_x"),
-        
+
         mean("acceleration_y").alias("avg_accel_y"),
         stddev("acceleration_y").alias("std_accel_y"),
         max("acceleration_y").alias("max_accel_y"),
-        
+
         mean("acceleration_z").alias("avg_accel_z"),
         stddev("acceleration_z").alias("std_accel_z"),
         max("acceleration_z").alias("max_accel_z"),
-        
+
         mean("gyro_x").alias("avg_gyro_x"),
         stddev("gyro_x").alias("std_gyro_x"),
-        
+
         mean("gyro_y").alias("avg_gyro_y"),
         stddev("gyro_y").alias("std_gyro_y"),
-        
+
         mean("gyro_z").alias("avg_gyro_z"),
         stddev("gyro_z").alias("std_gyro_z"),
-        
+
         mean("accuracy").alias("avg_accuracy"),
         stddev("accuracy").alias("std_accuracy"),
-        
+
         max("second").alias("second"),
     )
 
-    prediction = predict_new_data(model, metadata, processed_df.drop("bookingid"))
+    prediction = predict_new_data(processed_df.drop("bookingid"))
+
     prediction_message = {
         "bookingid": bookingID,
         "time": processed_df.select("second").first()[0],
@@ -150,19 +265,37 @@ def process_and_predict(bookingID, records, producer, model, metadata, spark):
         "label": int(prediction)
     }
     producer.send(KAFKA_TOPIC_OUTPUT, prediction_message)
-    print(f"Sent prediction: {prediction_message}")
 
 # Kafka Consumer
-def consume_messages(consumer, producer, model, metadata, spark):
+def consume_messages():
+    # CHECKER FOR MODEL print(f"@@@@@@@@@@@@@@@@@@@@@@@@ global_item in consume_messages: {get_model()}")
+
+    spark = None
+    consumer = None
+    producer = None
     try:
+        spark = generateSparkSession()
+        consumer = KafkaConsumer(
+            KAFKA_TOPIC_INPUT,
+            bootstrap_servers=[KAFKA_BROKER],
+            auto_offset_reset='latest',
+            value_deserializer=lambda m: json.loads(m.decode('utf-8'))
+        )
+        producer = KafkaProducer(
+            bootstrap_servers=[KAFKA_BROKER],
+            value_serializer=lambda v: json.dumps(v).encode('utf-8')
+        )
         for message in consumer:
+            global shared_model
+            # CHECKER FOR MODEL print(f"@@@@@@@@@@@@@@@@@@@@@@@@ global_item in consume_messages loop: {get_model()}")
+
             data = message.value
             bookingID = data["bookingid"]
             booking_timestamps[bookingID] = time.time()
 
             data_buffer[bookingID].append(data)
 
-            # print(f"Received data for bookingid: {bookingID}, buffer size: {len(data_buffer[bookingID])}")
+            print(f"Received data for bookingid: {bookingID}, buffer size: {len(data_buffer[bookingID])}")
 
             # Trigger and slide window
             if len(data_buffer[bookingID]) == WINDOW_SIZE:
@@ -174,64 +307,80 @@ def consume_messages(consumer, producer, model, metadata, spark):
 
                 # print(f"Processing bookingid: {bookingID}, but now records count from {WINDOW_SIZE} to {len(data_buffer[bookingID])}")
 
-                process_and_predict(bookingID, needed_records, producer, model, metadata, spark)
-                
-            cleanup_stale_records(producer, model, metadata, spark)  # Check for stale records
-                
+                process_and_predict(bookingID, needed_records, producer)
+
+            cleanup_stale_records(producer)  # Check for stale records
+
     except KeyboardInterrupt:
         print("Stopped consuming messages")
     finally:
-        consumer.close()
-        producer.close()
+        if consumer is not None:
+            consumer.close()
+        if producer is not None:
+            producer.close()
 
-def start_streaming(modelid):
-    
-    print(f"==================== START STREAMING FOR MODEL {modelid} ====================")
-    
-    print("Listening for streaming data...")
-    consumer = KafkaConsumer(
-        KAFKA_TOPIC_INPUT,
-        bootstrap_servers=[KAFKA_BROKER],
-        auto_offset_reset='latest',
-        value_deserializer=lambda m: json.loads(m.decode('utf-8'))
-    )
-    producer = KafkaProducer(
-        bootstrap_servers=[KAFKA_BROKER],
-        value_serializer=lambda v: json.dumps(v).encode('utf-8')
-    )
-    print("Connected to Kafka successfully")
+def refresh_model(modelid):
 
-    spark = SparkSession.builder \
-                        .appName("PySpark Streaming Processor") \
-                        .config("spark.hadoop.fs.s3a.endpoint", f"http://{MINIO_ADDRESS}:{MINIO_PORT}")\
-                        .config("spark.hadoop.fs.s3a.access.key", MINIO_USER) \
-                        .config("spark.hadoop.fs.s3a.secret.key", MINIO_PASSWORD) \
-                        .config("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem") \
-                        .config("spark.hadoop.fs.s3a.path.style.access", "true") \
-                        .config("spark.hadoop.fs.s3a.connection.ssl.enabled", "false") \
-                        .getOrCreate()
+    print(f"==================== refresh model: {modelid} ====================")
+
+    global shared_model
+
+    # CHECKER FOR MODEL print(f"@@@@@@@@@@@@@@@@@@@@@@@@ global_item in fast api call: {get_model()}")
     model, metadata = load_model_from_minio()
-        
-    consume_messages(consumer, producer, model, metadata, spark)
-    returnString = "Streaming process started"
-    
-    print(f"===================={returnString} ====================")
-    spark.stop()
+
+    set_model(model, metadata)
+    returnString = f"refresh model: {modelid} - Completed"
+
+    print(f"==================== {returnString} ====================")
 
     return returnString
 
 app = FastAPI()
 
-class Item(BaseModel):
+class post_item(BaseModel):
     modelid: str
 
-@app.post("/streaming")
-async def predict(data: Item):
-    result = start_streaming(data.modelid)
+@app.post("/refresh_model")
+async def refresh_model_call(data: post_item):
+    result = refresh_model(data.modelid)
     return {"status": "success", "message": f"{result}"}
 
-# Main function
+thread = None
+## HACKY way to start kafka using the thread from fast api, so that both thread can share the variable
+@app.get("/")
+async def root():
+    global thread
+    if thread is None:
+        thread = threading.Thread(target=start_kafka_consumer_task, args=(), daemon=True)
+        thread.start()
+    # kafka_thread = threading.Thread(target=start_kafka_consumer_task,args=(), daemon=True)
+    # kafka_thread.start()
+    # kafka_thread.join()
+    return {"message": "FastAPI server is running"}
+
+def start_fastapi():
+    print(f"starting fast api server on port {FAST_API_PORT}...")
+    uvicorn.run("streaming_data_handling:app", host="0.0.0.0", port=FAST_API_PORT, reload=False)
+
+    print(f"starting fast api server on port {FAST_API_PORT}...COMPLETED")
+import threading
+
+def start_kafka_consumer_task():
+    print(f"starting Kafka consumer...")
+    consume_messages()
+
+def main():
+
+    fastapi_thread = threading.Thread(target=start_fastapi,args=(), daemon=True)
+    # kafka_thread = threading.Thread(target=start_kafka_consumer_task,args=(), daemon=True)
+
+    fastapi_thread.start()
+    # kafka_thread.start()
+
+    fastapi_thread.join()
+    # kafka_thread.join()
+
+
 if __name__ == "__main__":
-    uvicorn.run("streaming_data_handling:app", host="0.0.0.0", port=FAST_API_PORT, reload=True)    
-    # result = start_streaming()
-    # print(f"Streaming process finished: {result}")
+    main()
+
